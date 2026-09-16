b257b447458d3232cc5660612877a45bec9485f7
leolinder-hash <leolinder074@gmail.com>
2026-09-16 15:44:38 +0200

    Add product constructor and build method

--- src/main/java/patterns/Product.java
+++ src/main/java/patterns/Product.java
@@ -10,13 +10,28 @@ public class Product {
     private LocalDate createdDate;
     private LocalDate modifiedDate;
 
+    private Product(
+            String id,
+            String name,
+            Category category,
+            int rating,
+            LocalDate createdDate,
+            LocalDate modifiedDate) {
+
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.rating = rating;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+
+    }
+
     public static class Builder {
         private String id;
         private String name;
         private Category category;
         private int rating;
-        private LocalDate createdDate;
-        private LocalDate modifiedDate;
 
         public Builder id(String id) {
             this.id = id;
@@ -28,14 +43,25 @@ public class Product {
             return this;
         }
 
-        public Builder category(Category category){
+        public Builder category(Category category) {
             this.category = category;
             return this;
         }
 
-        public Builder rating(int rating){
+        public Builder rating(int rating) {
             this.rating = rating;
             return this;
+        }
+
+        public Product build() {
+            return new Product(
+                    id,
+                    name,
+                    category,
+                    rating,
+                    LocalDate.now(),
+                    LocalDate.now()
+            );
         }
     }
 }
