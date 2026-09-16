c2e2b513c7c7bffa48a59a6d0fbc9fc0576340c5
leolinder-hash <leolinder074@gmail.com>
2026-09-16 13:58:09 +0200

    Add builder setter methods

--- src/main/java/patterns/Product.java
+++ src/main/java/patterns/Product.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Product {
     private String id;
     private String name;
-    private  Category category;
+    private Category category;
     private int rating;
     private LocalDate createdDate;
     private LocalDate modifiedDate;
@@ -17,5 +17,25 @@ public class Product {
         private int rating;
         private LocalDate createdDate;
         private LocalDate modifiedDate;
+
+        public Builder id(String id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder category(Category category){
+            this.category = category;
+            return this;
+        }
+
+        public Builder rating(int rating){
+            this.rating = rating;
+            return this;
+        }
     }
 }
