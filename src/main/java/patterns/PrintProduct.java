4fec2bf5867004418b2a9cf778d75393fba59160
leolinder-hash <leolinder074@gmail.com>
2026-09-17 14:31:34 +0200

    Update Product to include price

--- src/main/java/patterns/PrintProduct.java
+++ src/main/java/patterns/PrintProduct.java
@@ -1,11 +1,14 @@
 package patterns;
 
+import java.math.BigDecimal;
+
 public class PrintProduct {
     public static void main(String[] args) {
         Product product = new Product.Builder()
                 .id("1")
                 .name("Fridge")
                 .category(Category.ELECTRONICS)
+                .price(BigDecimal.valueOf(200))
                 .rating(5)
                 .build();
 
